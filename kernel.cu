b2c9937f891cc8b7d58ff0ae561fca67d3813ce0
TserJay <71320172+TserJay@users.noreply.github.com>
2025-05-13 15:06:09 +0800

    Add files via upload

--- kernel.cu
+++ kernel.cu
@@ -1,137 +1,105 @@
 ﻿
-#include <cuda_runtime.h>
-#include <device_launch_parameters.h>
-#include <stdio.h>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
 #include <cuda.h>
-#include <math.h>
 
-typedef float FLOAT;
-//output:
-//check pass!
-//GPU execution time : 57.808384 ms
-//CPU execution time : 275.917206 ms
+#include <stdio.h>
 
 
-/*cuda kernel function*/
-__global__ void vector_add(FLOAT *x, FLOAT *y, FLOAT *z, int N)
-{
-    /* 2d gird*/
-    int idx = blockDim.x * (blockIdx.x + blockIdx.y * gridDim.x) + threadIdx.x;
-    /* 1d gird*/
-    //int idx = blockDim.x * blockIdx.x + threadIdx.x;
-    if (idx < N)
-        z[idx] = x[idx] + y[idx];
-}
 
-void vector_add_cpu(FLOAT* x, FLOAT* y, FLOAT* z, int N) 
-{
-    for (size_t i = 0; i < N; i++)
+__global__ void reduce_baseline(const int * input, int *output, size_t n){
+    //由于只有一个block和thread，相当于串行的程序
+    int sum = 0;
+    //累加
+    for (int i = 0; i < n; i++)
     {
-        z[i] = x[i] + y[i];
+        sum += input[i];
     }
+    *output = sum;
+}
+  
+bool CheckResult(int* out, int groudtruth) 
+{
+    if (*out != groudtruth) {
+        return false;
+    }
+    return true;
 }
 
 int main()
-{   
-    int N = 100000000;
-    int nbytes = N * sizeof(FLOAT); //N个float所需要分配的内存大小
-
-    /* 1d block */
-    int bs = 256;
-
-    /* 2d grid */
-    int s = ceil(sqrt((N + bs - 1) / bs));
-
-    /* 构造一个 2D 网格，每行 s 个 block，共 s 行。*/
-    dim3 grid(s, s);
-
-    FLOAT * dx, * hx;
-    FLOAT * dy, * hy;
-    FLOAT * dz, * hz;
-
-    /* gpu分配内存空间 */
-    cudaMalloc((void**)&dx, nbytes);
-    cudaMalloc((void**)&dy, nbytes);
-    cudaMalloc((void**)&dz, nbytes);
-
-    /* 计时器，单位ms*/
+{
     float milliseconds = 0;
-    float milliseconds_cpu = 0;
 
-    /* cpu分配内存空间 */
-    hx = (FLOAT*)malloc(nbytes);
-    hy = (FLOAT*)malloc(nbytes);
-    hz = (FLOAT*)malloc(nbytes);
+    // const int N = 32 * 256 *256
+    const int N = 25600000;
+    cudaSetDevice(0);
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp, 0);
 
+    //const int blockSize = 256;
+    const int blockSize = 1;
 
-    /* init */
-    for (int i = 0; i < N; i++)
+    int GridSize = 1;
+
+    //分配内存和显存并初始化数据
+    int *a = (int*)malloc(N * sizeof(int));
+    int *d_a;
+    cudaMalloc((void**)&d_a, N * sizeof(int));
+
+    int* out = (int*)malloc(GridSize * sizeof(int));
+    int* d_out;
+    cudaMalloc((void**)&d_out, GridSize * sizeof(int));
+
+    //初始化
+    for (size_t i = 0; i < N; i++)
     {
-        hx[i] = 1;
-        hy[i] = 1;
+        a[i] = 1;
     }
 
-    /* cudaMemcpyHostToDevice or cudaMemcpyDeviceToHost*/
-    cudaMemcpy(dx, hx, nbytes, cudaMemcpyHostToDevice);
-    cudaMemcpy(dy, hy, nbytes, cudaMemcpyHostToDevice);
+    int groudtruth = N * 1;
+    //将初始化后的数据拷贝到GPU
+    cudaMemcpy(d_a, a, N * sizeof(int), cudaMemcpyHostToDevice);
+    //定义分配的block数量和threads的数量
+    dim3 grid(GridSize);
+    dim3 block(blockSize);
 
-
-    /*  创建事件函数 */
-    cudaEvent_t start, stop; // CUDA 事件 类型的变量
-    cudaEvent_t start_cpu, stop_cpu;
+    cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
-    cudaEventCreate(&start_cpu);
-    cudaEventCreate(&stop_cpu);
-    
-    /* 时间记录起点*/
     cudaEventRecord(start);
 
-    /* launchc kernel */
-    vector_add << <grid, bs >> > (dx, dy, dz, N);
+
+    //分配1个block和1个thread
+    reduce_baseline << <1, 1 >> > (d_a, d_out, N);
     cudaEventRecord(stop);
-    cudaEventSynchronize(stop); // 主机（CPU）等待，直到 stop 事件完成
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&milliseconds, start, stop);
+    //将结果拷贝回cpu并check正确性
+    cudaMemcpy(out, d_out, GridSize * sizeof(int), cudaMemcpyDeviceToHost);
 
-    cudaEventElapsedTime(&milliseconds, start, stop); //计算时间
+    printf("allcated %d block,data counts are %d\n", GridSize, N);
+    bool is_right = CheckResult(out, groudtruth);
+    if (is_right) {
+        printf("the ans is right\n");
 
-    /* copy GPU result to CPU*/
-    cudaMemcpy(hz, dz, nbytes, cudaMemcpyDeviceToHost);
-
-    /* CPU compute*/
-    FLOAT* hz_cpu = (FLOAT*)malloc(nbytes);
-  /*  vector_add_cpu(hx, hy, hz_cpu, N);*/
-
-    // 记录 CPU 计算时间
-    cudaEventRecord(start_cpu);
-    vector_add_cpu(hx, hy, hz_cpu, N);  // 执行 CPU 计算
-    cudaEventRecord(stop_cpu); // 结束计时
-    cudaEventSynchronize(stop_cpu);
-
-    // 计算时间差，单位为毫秒
-    cudaEventElapsedTime(&milliseconds_cpu, start_cpu, stop_cpu); //计算时间
-   
-
-
-    /* check GPU result with CPU*/
-    for (int i = 0; i < N; i++)
-    {
-        if (fabs(hz_cpu[i] - hz[i]) > 1e-6) 
-        {
-            printf("erro %d\n", i);
-        }
     }
-    printf("check pass!\n");
-    printf("GPU execution time: %f ms\nCPU execution time: %f ms\n  ", milliseconds, milliseconds_cpu);
- 
-   
-    cudaFree(dx);
-    cudaFree(dy);
-    cudaFree(dz);
+    else
+    {
+        printf("ans is wrong\n");
+        for (int i = 0; i < GridSize; i++)
+        {
+            printf("res per block : %lf ", out[i]);
+        }
+        printf("\n");
+        printf("groudtruth is: %f \n", groudtruth);
+    }
 
-    free(hx);
-    free(hy);
-    free(hz);
-    free(hz_cpu);
+    printf("reduce_baseline latency = %f ms\n", milliseconds);
+
+    cudaFree(d_a);
+    cudaFree(d_out);
+    free(a);
+    free(out);
 
 
     return 0;
