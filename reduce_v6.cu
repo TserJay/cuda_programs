e6c5ec9d3a9aa82a737f0a1f0870da867f0acacb
TserJay <71320172+TserJay@users.noreply.github.com>
2025-06-14 14:46:08 +0800

    Create reduce_v6.cu

---
+++ reduce_v6.cu
@@ -0,0 +1,198 @@
+
+
+#include <cuda_runtime.h>
+#include "device_launch_parameters.h"
+#include <cuda.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+//v4: 最后一个warp不用参与__syncthreads
+// 这个版本的reduce_v4使用了共享内存来进行归约操作，
+
+//allcated 100000 blocks, data count are 25600000
+//result is right
+//reduce_v4 latency = 5.658784 ms
+
+
+// no __syncwarp
+// reduce_v4 latency = 3.664032 ms
+// reduce_v4 latency = 3.427616 ms
+// reduce_v4 latency = 3.782432 ms
+
+
+
+// reduce_v5 latency = 4.144704 ms
+// reduce_v5 latency = 4.035072 ms
+
+// v5: 将所有的循环都展开
+
+
+// V0-V5 中每个kernel得到了的是每个block 所负责范围的结果（归约的结果）
+// 想要得到最终的结果，需要将每个block的结果进行再做reduce_sum操作，得到最终的结果。
+
+
+// V6:multi-block reduce final result by two pass
+// reduce_v6 latency = 8.146048 ms
+
+
+
+template<int blockSize>
+
+__device__ void BlockShareMemReduce(float* smem) {
+	// 对v4中的所有循环进行修改，将所有的循环都展开
+
+	if (blockSize >= 1024) {
+		if (threadIdx.x < 512) {
+			smem[threadIdx.x] += smem[threadIdx.x + 512];
+		}
+		__syncthreads();
+	}
+
+
+	if (blockSize >= 512) {
+		if (threadIdx.x < 256) {
+			smem[threadIdx.x] += smem[threadIdx.x + 256];
+		}
+		__syncthreads();
+	}
+
+	if (blockSize >= 256) {
+		if (threadIdx.x < 128) {
+			smem[threadIdx.x] += smem[threadIdx.x + 128];
+		}
+		__syncthreads();
+	}
+
+	if (blockSize >= 128) {
+		if (threadIdx.x < 64) {
+			smem[threadIdx.x] += smem[threadIdx.x + 64];
+		}
+		__syncthreads();
+	}
+
+	if (threadIdx.x < 32) {
+		volatile float* vshm = smem;
+		if (blockDim.x >= 64) {
+			vshm[threadIdx.x] += vshm[threadIdx.x + 32];
+		}
+		vshm[threadIdx.x] += vshm[threadIdx.x + 16];
+		vshm[threadIdx.x] += vshm[threadIdx.x + 8];
+		vshm[threadIdx.x] += vshm[threadIdx.x + 4];
+		vshm[threadIdx.x] += vshm[threadIdx.x + 2];
+		vshm[threadIdx.x] += vshm[threadIdx.x + 1];
+	}
+}
+
+template<int blockSize>
+__global__ void reduce_v6(float* d_in, float* d_out, int nums) {
+	__shared__ float smem[blockSize];
+	unsigned int tid = threadIdx.x; // 线程索引
+
+	// *2代表当前的 block 要处理 2*blockSize个数据
+	// eg blcoksize = 2, blockIdx.x = 1, when tid = 0, gtid = 4, gtid + blockSize = 6;when tid = 1, gtid = 5, gtid + blockSize = 7;
+	unsigned int gtid = blockIdx.x * blockDim.x  + threadIdx.x;
+	unsigned int total_threads_nums = gridDim.x * blockDim.x; // 总的线程数
+
+	float sum = 0.0f; // 存储最后的结果
+
+	// 基于V5改进优化：不显式指定一个线程处理2个元素，通过L100的循环来自动确定每个线程处理的元素个数
+	for (int32_t i = gtid; i < nums; i += total_threads_nums) {
+		sum += d_in[i];
+	}
+	smem[tid] = sum;
+	__syncthreads();
+
+	// comppute reduce sum in shared memory
+	BlockShareMemReduce<blockSize>(smem);
+
+
+	if (tid == 0) {
+		d_out[blockIdx.x] = smem[0]; // 将结果写回到全局内存
+	}
+	// 注意：这里的 smem[0] 是每个 block 的归约结果
+
+}
+
+
+
+bool CheckResult(float* out, float groudtruth, int n) {
+	if (*out != groudtruth) {
+		return false;
+	}
+	return true;
+}
+
+int main() {
+
+	float millisconds = 0;
+
+	//const int N = 32 * 1024 * 1024;
+	const int N = 25600000;
+
+	cudaSetDevice(0);
+	cudaDeviceProp deviceProp;
+	cudaGetDeviceProperties(&deviceProp, 0);  //获取cuda的设备信息
+
+	const int blockSize = 256; //每个block的线程数
+
+	int GridSize = std::min((N + blockSize - 1) / blockSize, deviceProp.maxGridSize[0]);
+	//int GridSize = 100000;
+
+	float* a = (float*)malloc(N * sizeof(float));
+	float* d_a; // 用来存储最终的结果
+	cudaMalloc((void**)&d_a, N * sizeof(float));
+	
+
+
+	float* out = (float*)malloc(GridSize * sizeof(float));
+	float* d_out;
+	float* part_out; // 用来存储每个block的结果
+	cudaMalloc((void**)&d_out, 1 * sizeof(float));
+	cudaMalloc((void**)&part_out, GridSize * sizeof(float));
+
+	for (int i = 0; i < N; i++)
+	{
+		a[i] = 1.0f; // 初始化数组a
+	}
+
+	cudaMemcpy(d_a, a, N * sizeof(float), cudaMemcpyHostToDevice); //将数据从主机复制到设备
+
+	dim3 Grid(GridSize); //每个Grid的block数
+	dim3 Block(blockSize); //每个Block的线程数
+
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	cudaEventRecord(start);
+	reduce_v6<blockSize> << <Grid, Block >> > (d_a, part_out, N);
+	reduce_v6<blockSize> << <1, Block >> > (part_out, d_out, GridSize);
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);// 等待事件完成
+	cudaEventElapsedTime(&millisconds, start, stop); //计算时间
+
+	cudaMemcpy(out, d_out,	1 * sizeof(float), cudaMemcpyDeviceToHost);//将结果从设备复制到主机
+	printf("allcated %d blocks, data count are %d\n", GridSize, N);
+	printf("%f\n", *out);
+	
+
+	float groudtruth = N * 1.0f;// 计算预期的结果
+	
+	bool is_right = CheckResult(out, groudtruth, GridSize);// 检查结果是否正确
+	if (is_right) {
+		printf("result is right\n");
+	}
+	else {
+		printf("result is wrong\n");
+	}
+	printf("reduce_v6 latency = %f ms\n", millisconds);
+
+	cudaFree(d_a);
+	cudaFree(d_out);
+	cudaFree(part_out);
+	free(a);
+	free(out);
+
+}
