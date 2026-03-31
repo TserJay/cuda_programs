07664abb66469d4da4340d55bb6bf52a0ad3f36e
TserJay <TserJay@outlook.com>
2026-03-31 13:08:50 +0000

    permute算子

--- permute/permute_naive.cu
+++ permute/permute_naive.cu
@@ -20,7 +20,6 @@ __global__ void permute_1(const float* input, float* output, int A, int B, int C
 }
 
 // 2. permute [0,1,2]->[1,0,2]
-
 __global__ void permute_2(const float* input, float* output, int A, int B, int C){
     int b = blockIdx.z;
     int a = blockIdx.y * blockDim.y + threadIdx.y;
@@ -29,6 +28,18 @@ __global__ void permute_2(const float* input, float* output, int A, int B, int C
     if (a >= A || c >= C) return;
     int in_idx = a * B * C + b * C + c;
     int out_idx = b * A * C + a * C +c;
+    output[out_idx] = input[in_idx];
+}
+
+// 3. permute [0,1,2]->[2,0,1] [C,A,B]
+__global__ void permute_3(const float* input, float* output, int A , int B, int C){
+    int c = blockIdx.z;
+    int a = blockIdx.y * blockDim.y + threadIdx.y;
+    int b = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (a >= A || b >= B) return;
+    int in_idx = a * B * C + b * C + c;
+    int out_idx = c * A * B + a * B + b; 
     output[out_idx] = input[in_idx];
 }
 
@@ -52,6 +63,19 @@ void permute_2_cpu(const float* input, float* output, int A, int B, int C) {
             for (int c = 0; c < C; c++) {
                 int in_idx = a * B * C + b * C + c;
                 int out_idx = b * A * C + a * C +c;
+                output[out_idx] = input[in_idx];
+            }
+        }
+    }
+}
+
+// [2,0,1] [C,A,B]
+void permute_3_cpu(const float* input, float* output, int A, int B, int C) {
+    for (int c = 0; c < C; c++) {
+        for (int a = 0; a < A; a++) {
+            for (int b = 0; b < B; b++) {
+                int in_idx = a * B * C + b * C + c;
+                int out_idx = c * A * B + a * B + b; 
                 output[out_idx] = input[in_idx];
             }
         }
@@ -95,9 +119,12 @@ int main() {
     float* d_input = nullptr; 
     float* d_output_1 = nullptr;
     float* d_output_2 = nullptr;
+    float* d_output_3 = nullptr;
     cudaMalloc(&d_input, n * sizeof(float));
     cudaMalloc(&d_output_1, n * sizeof(float));
     cudaMalloc(&d_output_2, n * sizeof(float));
+    cudaMalloc(&d_output_3, n * sizeof(float));
+
 
 
     // cpy Host to Device
@@ -114,9 +141,16 @@ int main() {
         (A + BlockDim.y -1) / BlockDim.y,
         B
     );
+    dim3 GridDim3(
+        (B + BlockDim.x -1) / BlockDim.x,
+        (A + BlockDim.y -1) / BlockDim.y,
+        C
+    );
+
     // gpu
     permute_1<<<GridDim1,BlockDim>>>(d_input, d_output_1, A, B, C);
     permute_2<<<GridDim2,BlockDim>>>(d_input, d_output_2, A, B, C);
+    permute_3<<<GridDim3,BlockDim>>>(d_input, d_output_3, A, B, C);
     cudaDeviceSynchronize();
 
     // cpy Device to Host
@@ -136,6 +170,17 @@ int main() {
     // cpu 
     permute_2_cpu(h_input, h_output_cpu, A, B, C);
     printf("2.permute [0,1,2]->[1,0,2]: ");
+    if(verify_result(h_output, h_output_cpu, n)){
+        printf("True\n");
+    }else{
+        printf("false\n");
+    }
+
+
+    cudaMemcpy(h_output, d_output_3, n*sizeof(float), cudaMemcpyDeviceToHost);
+    // cpu 
+    permute_3_cpu(h_input, h_output_cpu, A, B, C);
+    printf("3.permute [0,1,2]->[2,0,1]: ");
     if(verify_result(h_output, h_output_cpu, n)){
         printf("True\n");
     }else{
